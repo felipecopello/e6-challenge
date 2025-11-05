aa5210aebbf94a85eef2f140c65aeb43b2c2868c
felipe <felipecopello94@gmail.com>
2025-11-05 14:22:35 -0300

    create project

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("java")
+    id("jacoco")
+    id("com.diffplug.spotless") version "6.23.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+jacoco {
+    toolVersion = "0.8.12"
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.required.set(true)
+        html.outputLocation.set(layout.buildDirectory.dir("reports/jacoco"))
+    }
+}
+
+spotless {
+    java {
+        target("src/**/*.java")
+        removeUnusedImports()
+        googleJavaFormat("1.21.0")
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.slf4j:slf4j-api:2.0.12")
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+    testCompileOnly("org.projectlombok:lombok:1.18.34")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "e6-challenge"
+
